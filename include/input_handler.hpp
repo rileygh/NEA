123a945244af73bc262628b3306779db02629467
Jade <jadedevofficial@gmail.com>
2024-06-06 19:06:37 +0100

    input handler

---
+++ include/input_handler.hpp
@@ -0,0 +1,19 @@
+#ifndef INPUTHANDLER_HPP
+#define INPUTHANDLER_HPP
+
+#include <SDL2/SDL.h>
+#include <map>
+
+class InputHandler {
+    public:
+    InputHandler();
+    ~InputHandler();
+
+    void handle();
+    bool key_pressed(SDL_Keycode key);
+
+    private:
+    std::map<SDL_Keycode, bool> key_states;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 
-int main() {
-    const int img_width = 1920;
-    const int img_height = 1080;
+#include <SDL2/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
 
-    // render ppm (ref. example at https://en.wikipedia.org/wiki/Netpbm)
-    std::cout << "P3\n" << img_width << ' ' << img_height << "\n255\n"; // P3 indicates RGB colour image in ASCII, followed by width, height, then max value for a colour
-    for (int i = 0; i < img_height; i++) {
-        for (int j = 0; j < img_width; j++) {
-            // example gradient
-            int r = 255 * i / img_height;
-            int g = 0;
-            int b = 255 * j / img_width;
+#include "input_handler.hpp"
 
-            std::cout << r << ' ' << g << ' ' << b << ' ';
-        }
+#define WIDTH 500
+#define HEIGHT 500
+
+int main(int argc, char **argv) {
+    if (SDL_InitSubSystem(SDL_INIT_VIDEO) != 0) {
+        std::cout << "error initialising: %s\n", SDL_GetError();
+        return 0;
     }
-    std::clog << "done!";
+
+    SDL_Window *window = SDL_CreateWindow("meow", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+
+    bool mainloop = true;
+    InputHandler handler;
+
+    while (mainloop) {
+        handler.handle();
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
