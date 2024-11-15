f593fb5abf6fd070ac2a1ce49935eedef92e515f
Jade <jadedevofficial@gmail.com>
2024-11-15 17:10:50 +0000

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,10 @@ int main(int argc, char **argv) {
     float temp_fov = 0.0;
 
     if (!glfwInit()) { return 1; }
-
+    
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "test", nullptr, nullptr);
     if (window == nullptr) { return 1; }
 
-    if (!glfwInit()) { return 1; }
     GLFWwindow* window = glfwCreateWindow(1280, 720, "test", nullptr, nullptr);
     
     if (window == nullptr) { return 1; }
