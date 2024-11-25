10d95e26cab802a0dc7b223921aef48fa1c84d96
Riley <riley.s.hewitt@proton.me>
2024-11-25 13:41:50 +0000

    i forgot what these do

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@
 #define WIDTH 960
 #define HEIGHT 540
 
-
 void glfw_error_callback(int error, const char* description) {
     std::cerr << "GLFW error: " << error << ": " << description << '\n';
 }
